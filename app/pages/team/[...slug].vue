15f7a96c6890c8685b21774353856a5a62bd4903
c1chy1 <cichy08081987pp@gmail.com>
2025-09-16 16:35:54 +0200

    Refactor team page: replace back button with `NuxtLink` for improved navigation, add `handleClick` method for reliable fallback on navigation errors, and clean up unused debug logs.

--- app/pages/team/[...slug].vue
+++ app/pages/team/[...slug].vue
@@ -13,18 +13,22 @@
         <div
             :class="`bg-(--color-${person?.meta.bg})`"
             class="sm:w-1/2 h-1/2 sm:h-full flex flex-col justify-center px-4 space-y-4 lg:px-6 lg:space-y-6 xl:space-y-8 xl:px-8 2xl:space-y-12 2xl:px-20">
-          <button
-              class="w-12 h-12 bg-black/70 hover:bg-black/100 flex justify-center items-center rounded-full absolute right-12 bottom-0 transition-all z-50"
-              @click="$router.back()">
+          <NuxtLink
+              :to="localePath('/')"
+              class="w-12 h-12 flex justify-center items-center rounded-full absolute right-12 bottom-0 bg-black/90 transition-opacity duration-300 opacity-80 hover:opacity-100 z-50"
+              @click="handleClick"
+          >
             <UIcon class="text-lg sm:text-3xl text-(--color-jm-primary-brown) transition-all z-50"
                    name="i-mdi-close-thick"/>
-          </button>
+          </NuxtLink>
+
+
           <h1 class="text-lg uppercase font-extrabold" v-html="person?.meta.name"/>
           <p class="font-light text-base!"> {{ person?.description }}</p>
           <UContainer class="relative pt-0 z-10 text-center">
             <NuxtLink href="https://calendar.app.google/rBDjAnPNYEQpfMvJ9" target="_blank">
               <button
-                  :class="['border-2 rounded', 'text-xs lg:text-base px-4 py-2 uppercase font-bold', person?.meta.button]">
+                  :class="['border-2 rounded', 'text-xs lg:text-base px-4 py-2 uppercase font-bold cursor-pointer', person?.meta.button, person?.meta.buttonHover]">
                 Lerne uns kennen
               </button>
             </NuxtLink>
@@ -42,16 +46,31 @@ definePageMeta({
   layout: 'minimal'
 });
 
-
+const localePath = useLocalePath()
 const teamStore = useTeamStore()
 const route = useRoute()
-const {slug} = route.params;
+const router = useRouter()
 
-console.log('Current route path:', slug)
+const {slug} = route.params;
 
 await teamStore.fetchPerson(slug)
 
 const person = computed(() => teamStore.person)
 
-console.log('Person from store:', person.value)
+
+const handleClick = async (event: Event) => {
+  event.preventDefault()
+
+  try {
+    await navigateTo(localePath('/'))
+  } catch (error) {
+    console.error('Navigation failed, fallback to localePath')
+    await router.push(localePath('/'))
+  }
+}
+
+
 </script>
+
+
+
